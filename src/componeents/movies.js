a71f37c844e6f860a2b9da62194240a3d0c2fa7c
Bartosz Wiaderny <bwiaderny@technikumlodz.pl>
2026-09-23 15:06:05 +0200

    dodanie tablicy obiektow movies ktora zawiera tytuly filmow oraz informacja o nich

---
+++ src/componeents/movies.js
@@ -0,0 +1,66 @@
+const movies = [
+ {
+ id: 1,
+ title: "Zielona mila",
+ genre: "Dramat",
+ duration: 11280,
+ description: "Emerytowany strażnik więzienny opowiada przyjaciółce o niezwykłym mężczyźnie, którego skazano na śmierć za zabójstwo dwóch 9-letnich dziewczynek.",
+ poster: "../assets/zielonaMila.png",
+ showtimes: ["14:00", "17:30"]
+ },
+ {
+ id: 2,
+ title: "Forrest Gump",
+ genre: "Dramat/Komedia",
+ duration: 8520,
+ description: "Historia życia Forresta, chłopca o niskim ilorazie inteligencji z niedowładem kończyn, który staje się miliarderem i bohaterem wojny w Wietnamie.",
+ poster: "../assets/forestGump.png",
+ showtimes: ["19:00", "20:30"]
+ },
+ {
+ id: 3,
+ title: "Matrix",
+ genre: "Sci-Fi",
+ duration: 8160,
+ description: "Haker komputerowy Neo dowiaduje się od tajemniczych rebeliantów, że świat, w którym żyje, jest tylko obrazem przesyłanym do jego mózgu przez roboty.",
+ poster: "../assets/matrix.png",
+ showtimes: ["12:00", "15:30"]
+ },
+ {
+ id: 4,
+ title: "Milczenie owiec",
+ genre: "Thriller",
+ duration: 7080,
+ description: "Seryjny morderca i inteligentna agentka łączą siły, by znaleźć przestępcę obdzierającego ze skóry swoje ofiary.",
+ poster: "../assets/milczenieOwiec.png",
+ showtimes: ["17:00", "19:30"]
+ },
+ {
+ id: 5,
+ title: "Pulp Fiction",
+ genre: "Ganksterski",
+ duration: 9240,
+ description: "Przemoc i odkupienie w opowieści o dwóch płatnych mordercach pracujących na zlecenie mafii, żonie gangstera, bokserze i parze okradającej ludzi w restauracji.",
+ poster: "../assets/pulpFiction.png",
+ showtimes: ["20:00", "23:30"]
+ },
+ {
+ id: 6,
+ title: "Podziemny krąg",
+ genre: "Psychologiczny/Thliller",
+ duration: 8340,
+ description: "Cierpiący na bezsenność mężczyzna poznaje gardzącego konsumpcyjnym stylem życia Tylera Durdena, który jest jego zupełnym przeciwieństwem.",
+ poster: "../assets/podziemnyKrag.png",
+ showtimes: ["14:40", "18:30"]
+ },
+{
+ id: 7,
+ title: "Nietykalni",
+ genre: "Psychologiczny/Thliller",
+ duration: 6720,
+ description: "Sparaliżowany milioner zatrudnia do opieki młodego chłopaka z przedmieścia, który właśnie wyszedł z więzienia.",
+ poster: "../assets/matrix.png",
+ showtimes: ["16:00", "19:30"]
+ },
+]
+export default movies;
