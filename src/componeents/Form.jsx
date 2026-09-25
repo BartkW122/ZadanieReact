1fcdd972ea7e57c2e7b70ce075c57c4d1aba2afc
Bartosz Wiaderny <bwiaderny@technikumlodz.pl>
2026-09-25 08:33:44 +0200

    dodanie walidacji pol formularza

--- src/componeents/Form.jsx
+++ src/componeents/Form.jsx
@@ -1,22 +1,46 @@
-export default function Form(){
-    return(
-        <form>
-            <label>
-                Imie:
-                <input type="text" placeholder="podaj imie..."/>
-            </label>
+import { Children } from "react";
 
-            <label>
-                nazwisko:
-                <input type="text" placeholder="podaj naziwsko..."/>
-            </label>
+export default function Form() {
+  function walidacja(e) {
+    e.preventDefault();
 
-            <label>
-                email:
-                <input type="email" placeholder="podaj email..."/>
-            </label>
+    const formElements = e.target.querySelectorAll("input");
+    let allFilled = true;
 
-            <button type="submit">Zaloguj się</button>
-        </form>
-    )
+    for (const input of formElements) {
+      if (input.value.trim() === "") {
+        alert("Wypełnij wszystkie pola!");
+        allFilled = false;
+        break;
+      }
+    }
+
+    if (allFilled) {
+      for (const input of formElements) {
+        input.value = "";
+      }
+      console.log("Formularz został wyczyszczony.");
+    }
+  }
+
+  return (
+    <form onSubmit={walidacja}>
+      <label>
+        Imie:
+        <input name="name" type="text" placeholder="podaj imie..." />
+      </label>
+
+      <label>
+        nazwisko:
+        <input name="surname" type="text" placeholder="podaj naziwsko..." />
+      </label>
+
+      <label>
+        email:
+        <input name="email" type="email" placeholder="podaj email..." />
+      </label>
+
+      <button type="submit">Zaloguj się</button>
+    </form>
+  );
 }
